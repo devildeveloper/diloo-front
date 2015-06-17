1051083a9daa0ac0e60179f085a085e6e7f43f7f
Miguel <Dev@iMac-de-Hector.local>
2015-06-17 11:53:11 -0500

    exporting data to excel

--- js/main.js
+++ js/main.js
@@ -2,8 +2,22 @@ angular.module('diloo',['ngSails'])
 		.config(['$sailsProvider', function ($sailsProvider) {
     		$sailsProvider.url = 'http://54.200.192.15:1337/';
 		}])
+		.factory('Excel',function($window){
+	        var uri='data:application/vnd.ms-excel;base64,',
+	            template='<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
+	            base64=function(s){return $window.btoa(unescape(encodeURIComponent(s)));},
+	            format=function(s,c){return s.replace(/{(\w+)}/g,function(m,p){return c[p];})};
+	        return {
+	            tableToExcel:function(tableId,worksheetName){
+	                var table=document.getElementById(tableId),
+	                    ctx={worksheet:worksheetName,table:table.innerHTML},
+	                    href=uri+base64(format(template,ctx));
+	                return href;
+	            }
+	        };
+	    })		
 		.controller('TicketController',TicketController);
-function TicketController($scope,$sails){
+function TicketController($scope,$sails,Excel,$timeout){
 	$scope.tickets={
 		data: [],
 		total:0
@@ -50,7 +64,7 @@ function TicketController($scope,$sails){
 					console.log(status)
 				});		
 		//changing ticket state
-		$scope.ticketStatus=!$scope.ticketStatus;
+		$scope.ticketStatus=true;
 	}
 	$scope.closeOpenTicket=function(){
 		$scope.ticketStatus=!$scope.ticketStatus;
@@ -72,5 +86,9 @@ function TicketController($scope,$sails){
 		$scope.jump <= 0 ? $scope.jump =0 : $scope.jump-= 25 ;
 		$scope.loadMessages();
 		console.log($scope.jump)
-	}	
+	}
+  $scope.exportToExcel=function(tableId){ // ex: '#my-table'
+        var exportHref=Excel.tableToExcel(tableId,'sheet name');
+            $timeout(function(){location.href=exportHref;},100); // trigger download
+    }
 }
